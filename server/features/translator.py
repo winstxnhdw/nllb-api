33596545a0c6dcd5cf678ef825fa448733284dd9
winstxnhdw <winstxnhdw@gmail.com>
2023-06-20 07:33:48 +0800

    feat: migrate to `ctranslate2` from `torch`

--- server/features/translator.py
+++ server/features/translator.py
@@ -1,6 +1,6 @@
-import torch
-from transformers import (AutoModelForSeq2SeqLM, NllbTokenizer,
-                          PreTrainedTokenizer, pipeline)
+from ctranslate2 import Translator as CTranslator
+from huggingface_hub import snapshot_download
+from transformers import NllbTokenizerFast, PreTrainedTokenizerFast
 
 
 class Translator:
@@ -12,18 +12,19 @@ class Translator:
     Attributes
     ----------
     model_name (str) : the name of the model
+    translator_model_path (str) : the path to the model
     tokeniser (PreTrainedTokenizer) : the tokeniser
-    model (AutoModelForSeq2SeqLM) : the model
+    translator (Translator) : the ctranslate2 translator object
 
     Methods
     -------
     translate(input: str, source_language: str, target_language: str) -> str | None
         translate the input from the source language to the target language
     """
-    model_name = 'facebook/nllb-200-distilled-600M'
-    tokeniser: PreTrainedTokenizer = NllbTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
-
+    model_name = 'JustFrederik/nllb-200-distilled-1.3B-ct2-int8'
+    translator_model_path = snapshot_download(model_name)
+    tokeniser: PreTrainedTokenizerFast = NllbTokenizerFast.from_pretrained(model_name)
+    translator = CTranslator(translator_model_path)
 
     @classmethod
     def translate(cls, text: str, source_language: str, target_language: str) -> str | None:
@@ -42,13 +43,9 @@ class Translator:
         -------
         translated_text (str | None) : the translated text
         """
-        translator = pipeline(
-            'translation',
-            cls.model,
-            tokenizer=cls.tokeniser,
-            src_lang=source_language,
-            tgt_lang=target_language,
-        )
+        cls.tokeniser.set_src_lang_special_tokens(source_language)
+        source = cls.tokeniser.convert_ids_to_tokens(cls.tokeniser.encode(text))
+        results = cls.translator.translate_batch([source], target_prefix=[[target_language]])
+        target = results[0].hypotheses[0][1:]
 
-        output = translator(text, max_length=1000)[0]  # type: ignore
-        return output.get('translation_text')          # type: ignore
+        return cls.tokeniser.decode(cls.tokeniser.convert_tokens_to_ids(target))
