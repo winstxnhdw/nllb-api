7646901d1c3698e6561ed573023426a87973a650
winstxnhdw <winstxnhdw@gmail.com>
2023-08-07 21:02:09 +0800

    refactor: update `CTranslate2` typings

--- server/typings/ctranslate2/__init__.pyi
+++ server/typings/ctranslate2/__init__.pyi
@@ -1,6 +1,6 @@
 # pylint: skip-file
 
-from typing import Literal
+from typing import Callable, Literal, overload
 
 ComputeTypes = Literal[
     'default',
@@ -15,8 +15,26 @@ ComputeTypes = Literal[
 ]
 
 
-class TranslatorResults:
+class GenerationStepResult:
+    batch_id: int
+    is_last: bool
+    log_prob: float | None
+    step: int
+    token: str
+    token_id: int
+
+
+class TranslationResult:
+    attention: list[list[str]]
     hypotheses: list[list[str]]
+    scores: list[float]
+
+
+class AsyncTranslationResult:
+
+    def done(self) -> bool: ...
+
+    def result(self) -> TranslationResult: ...
 
 
 class Translator:
@@ -26,8 +44,84 @@ class Translator:
         model_path: str,
         device: Literal['cpu', 'cuda', 'auto'] = 'cpu',
         *,
+        device_index: str | dict[str, str] = 'default',
         compute_type: ComputeTypes = 'default',
+        inter_threads: int = 1,
+        intra_threads: int = 0,
+        max_queued_batches: int = 0,
+        files: object = None,
     ) -> None: ...
 
 
-    def translate_batch(self, tokens: list[list[str]], *, target_prefix: list[list[str]]) -> list[TranslatorResults]: ...
+    @overload
+    def translate_batch(
+        self,
+        source: list[list[str]],
+        target_prefix: list[list[str] | None] | None = None,
+        *,
+        max_batch_size: int = 0,
+        batch_type: str = 'examples',
+        asynchronous: Literal[False] = False,
+        beam_size: int = 2,
+        patience: float = 1,
+        num_hypotheses: int = 1,
+        length_penalty: float = 1,
+        coverage_penalty: float = 0,
+        repetition_penalty: float = 1,
+        no_repeat_ngram_size: int = 0,
+        disable_unks: bool = False,
+        supress_sequences: list[list[str]] | None = None,
+        end_token: str | list[str] | list[int] | None = None,
+        return_end_token: bool = False,
+        prefix_bias_beta: float = 0,
+        max_input_length: int = 1024,
+        max_decoding_length: int = 256,
+        min_decoding_length: int = 1,
+        use_vmap: bool = False,
+        return_scores: bool = False,
+        return_attention: bool = False,
+        return_alternatives: bool = False,
+        min_alternative_expansion_prob: float = 0,
+        sampling_topk: int = 1,
+        sampling_topp: float = 1,
+        sampling_temperature: float = 1,
+        replace_unknowns: bool = False,
+        callback: Callable[[GenerationStepResult], bool] | None = None
+    ) -> list[TranslationResult]: ...
+
+
+    @overload
+    def translate_batch(
+        self,
+        source: list[list[str]],
+        target_prefix: list[list[str] | None] | None = None,
+        *,
+        max_batch_size: int = 0,
+        batch_type: str = 'examples',
+        asynchronous: Literal[True],
+        beam_size: int = 2,
+        patience: float = 1,
+        num_hypotheses: int = 1,
+        length_penalty: float = 1,
+        coverage_penalty: float = 0,
+        repetition_penalty: float = 1,
+        no_repeat_ngram_size: int = 0,
+        disable_unks: bool = False,
+        supress_sequences: list[list[str]] | None = None,
+        end_token: str | list[str] | list[int] | None = None,
+        return_end_token: bool = False,
+        prefix_bias_beta: float = 0,
+        max_input_length: int = 1024,
+        max_decoding_length: int = 256,
+        min_decoding_length: int = 1,
+        use_vmap: bool = False,
+        return_scores: bool = False,
+        return_attention: bool = False,
+        return_alternatives: bool = False,
+        min_alternative_expansion_prob: float = 0,
+        sampling_topk: int = 1,
+        sampling_topp: float = 1,
+        sampling_temperature: float = 1,
+        replace_unknowns: bool = False,
+        callback: Callable[[GenerationStepResult], bool] | None = None
+    ) -> list[AsyncTranslationResult]: ...
