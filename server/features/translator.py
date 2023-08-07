99277fd9d009ad8debabde333409e1c3146ea10d
winstxnhdw <winstxnhdw@gmail.com>
2023-08-07 20:46:07 +0800

    perf: use all available threads to download model

--- server/features/translator.py
+++ server/features/translator.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from ctranslate2 import Translator as CTranslator
 from huggingface_hub import snapshot_download
 from transformers.models.nllb import NllbTokenizerFast
@@ -22,11 +24,12 @@ class Translator:
         translate the input from the source language to the target language
     """
     model_name = 'winstxnhdw/nllb-200-distilled-1.3B-ct2-int8'
-    translator_model_path = snapshot_download(model_name)
+    translator_model_path = snapshot_download(model_name, max_workers=16)
     tokeniser: NllbTokenizerFast = NllbTokenizerFast.from_pretrained(model_name)
     translator = CTranslator(translator_model_path, compute_type='auto')
 
     @classmethod
+    @cache
     def translate(cls, text: str, source_language: str, target_language: str) -> str:
         """
         Summary
@@ -48,7 +51,8 @@ class Translator:
         if isinstance(tokens := cls.tokeniser.convert_ids_to_tokens(cls.tokeniser.encode(text)), str):
             tokens = [tokens]
 
-        results = cls.translator.translate_batch([tokens], target_prefix=[[target_language]])
-        target: list[str] = results[0].hypotheses[0][1:]
+        result = cls.translator.translate_batch([tokens], [[target_language]])[0]
 
-        return cls.tokeniser.decode(cls.tokeniser.convert_tokens_to_ids(target))
+        return cls.tokeniser.decode(
+            cls.tokeniser.convert_tokens_to_ids(result.hypotheses[0][1:])
+        )
