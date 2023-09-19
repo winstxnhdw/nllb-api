b18f0774013b6d53939cf12ffbab2807adb9536c
winstxnhdw <winstxnhdw@gmail.com>
2023-09-20 02:30:46 +0800

    refactor: fix plain text response

--- server/api/v1/index.py
+++ server/api/v1/index.py
@@ -1,13 +1,15 @@
+from typing import Literal
+
 from fastapi.responses import PlainTextResponse
 
 from server.api.v1 import v1
 
 
-@v1.get('/', response_class=PlainTextResponse)
+@v1.get('/', response_model=Literal['Welcome to v1 of the API!'])
 def index():
     """
     Summary
     -------
     the `/` route
     """
-    return 'Welcome to v1 of the API!'
+    return PlainTextResponse('Welcome to v1 of the API!')
--- server/api/v2/index.py
+++ server/api/v2/index.py
@@ -1,13 +1,15 @@
+from typing import Literal
+
 from fastapi.responses import PlainTextResponse
 
 from server.api.v2 import v2
 
 
-@v2.get('/', response_class=PlainTextResponse)
+@v2.get('/', response_model=Literal['Welcome to v1 of the API!'])
 def index():
     """
     Summary
     -------
     the `/` route
     """
-    return 'Welcome to v2 of the API!'
+    return PlainTextResponse('Welcome to v1 of the API!')
