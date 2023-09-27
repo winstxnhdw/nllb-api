316a8fd421fa857b954e1951471fdb6b0531dc4c
winstxnhdw <winstxnhdw@gmail.com>
2023-09-28 05:18:32 +0800

    feat: add test for non-latin languages

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -20,6 +20,17 @@ def test_generate(client: TestClient):
 
     assert response.text == '¡Hola, mundo!\n'
 
+
+def test_generate_from_chinese(client: TestClient):
+    response = client.post('/v2/translate', json={
+        'text': '我是一名软件工程师！',
+        'source': 'zho_Hans',
+        'target': 'spa_Latn'
+    })
+
+    assert response.text == '¡Soy ingeniero de software!\n'
+
+
 def test_generate_stream(client: TestClient):
     english_texts = ['Hello, world!', 'Today is a good day.', 'Hopefully, it will stay that way.']
     spanish_texts = ['¡Hola, mundo!', 'Hoy es un buen día.', 'Con suerte, seguirá así.']
