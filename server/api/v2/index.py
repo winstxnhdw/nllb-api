536b3a941d0b831e8666154f13676408c2528a69
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2024-02-23 17:04:29 +0800

    fix: update index response

--- server/api/v2/index.py
+++ server/api/v2/index.py
@@ -5,11 +5,11 @@ from fastapi.responses import PlainTextResponse
 from server.api.v2 import v2
 
 
-@v2.get('/', response_model=Literal['Welcome to v1 of the API!'])
+@v2.get('/', response_model=Literal['Welcome to v2 of the API!'])
 def index():
     """
     Summary
     -------
     the `/` route
     """
-    return PlainTextResponse('Welcome to v1 of the API!')
+    return PlainTextResponse('Welcome to v2 of the API!')
