2663ee5660c44bfacef92ce734f566aa9b9ca9ee
winstxnhdw <winstxnhdw@gmail.com>
2024-02-27 06:10:28 +0800

    chore: remove redundant config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,8 +6,6 @@ from server.config import Config
 wsgi_app = f'{initialise.__module__}:{initialise.__name__}()'
 reload = False
 accesslog = '-'
-limit_request_fields = 1
-limit_request_field_size = 2048
 preload_app = True
 bind = f'0.0.0.0:{Config.server_port}'
 workers = Config.worker_count
