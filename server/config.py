adf8a9783cb9375c6906438f636df58e72edee6b
winstxnhdw <winstxnhdw@gmail.com>
2024-06-06 20:30:56 +0800

    refactor: use a real singleton

--- server/config.py
+++ server/config.py
@@ -1,10 +1,29 @@
-from typing import TypeVar
+from functools import lru_cache
+from typing import Callable, TypeVar
 
 from pydantic_settings import BaseSettings
 
 T = TypeVar('T')
 
 
+def cache(user_function: Callable[[type[T]], T]) -> Callable[[type[T]], T]:
+    """
+    Summary
+    -------
+    a specific decorator to cache the result of a function
+
+    Parameters
+    ----------
+    user_function (Callable[[type[T]], T]) : the function to cache
+
+    Returns
+    -------
+    wrapper (Callable[[type[T]], T]) : the wrapper function
+    """
+    return lru_cache(maxsize=None)(user_function)
+
+
+@cache
 def singleton(cls: type[T]) -> T:
     """
     Summary
@@ -31,7 +50,7 @@ class Config(BaseSettings):
 
     Attributes
     ----------
-    port (int) : the port to run the server on
+    server_port (int) : the port to run the server on
     server_root_path (str) : the root path for the server
     worker_count (int) : the number of workers to use
     use_cuda (bool) : whether to use CUDA for inference
