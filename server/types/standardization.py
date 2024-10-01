228b4b3b664896cb45357f27d253ea365eecc272
JulianKropp <JulianKropp@users.noreply.github.com>
2024-10-01 18:19:51 +0000

    :sparkles: Add language-codes

---
+++ server/types/standardization.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+type Standardization = Literal[
+    'FLORES-200',
+    'ISO-639-3',
+    'ISO-15924',
+    'name',
+    'ISO-639-1',
+    'ISO-639-2/B',
+    'IETF-BCP-47',
+    'RFC-5646',
+    'CLDR',
+    'Ethnologue',
+    'SIL',
+    'LC',
+]
