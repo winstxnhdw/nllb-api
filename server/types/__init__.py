891782eeff14e66d11718b82fe5552d9d0449f9f
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2024-11-06 20:06:26 +0000

    refactor: move `AppState` into `types` (#247)

--- server/types/__init__.py
+++ server/types/__init__.py
@@ -1,3 +1,4 @@
 from server.types.compute_types import ComputeTypes as ComputeTypes
 from server.types.devices import Devices as Devices
 from server.types.languages import Languages as Languages
+from server.types.state import AppState as AppState
