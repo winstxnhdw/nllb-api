8adfb43ceedc4d71075f5dbb564d321af2ffebef
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-06-08 00:06:35 +0100

    docs: add docstring to `/health` endpoint

--- server/api/health.py
+++ server/api/health.py
@@ -4,4 +4,9 @@ from litestar.status_codes import HTTP_204_NO_CONTENT
 
 @get('/health', sync_to_thread=False, status_code=HTTP_204_NO_CONTENT)
 def health() -> None:
+    """
+    Summary
+    -------
+    the `/health` will only return a 204 status code
+    """
     return
