dab8322b22e0ecb4ebf8629237173eaed123bae0
winstxnhdw <winstxnhdw@gmail.com>
2025-06-24 19:53:56 +0100

    perf: use language detection ensemble

--- server/features/language_detector.py
+++ server/features/language_detector.py
@@ -2,9 +2,11 @@ from unittest.mock import create_autospec
 
 from fasttext import load_model
 from fasttext.FastText import _FastText as FastText
+from lingua import Language as LinguaLanguage
+from lingua import LanguageDetector as LinguaLanguageDetector
+from lingua import LanguageDetectorBuilder
 
-from server.typedefs import Score
-from server.typedefs.language import Language
+from server.typedefs import Language, Score
 from server.utils import huggingface_file_download
 
 
@@ -20,10 +22,89 @@ class LanguageDetector:
         detect the language of the input text
     """
 
-    __slots__ = ('model',)
+    __slots__ = ('fast_model', 'lingua_languages', 'lingua_model')
 
-    def __init__(self, model: FastText) -> None:
-        self.model = model
+    def __init__(self, fast_model: FastText, lingua_model: LinguaLanguageDetector) -> None:
+        self.fast_model = fast_model
+        self.lingua_model = lingua_model
+        self.lingua_languages = [
+            None,
+            'afr_Latn',
+            'als_Latn',
+            'arb_Latn',
+            'hye_Armn',
+            'azj_Latn',
+            'eus_Latn',
+            'bel_Cyrl',
+            'ben_Beng',
+            'nob_Latn',
+            'bos_Latn',
+            'bul_Cyrl',
+            'cat_Latn',
+            'zho_Hans',
+            'hrv_Latn',
+            'ces_Latn',
+            'dan_Latn',
+            'nld_Latn',
+            'eng_Latn',
+            'epo_Latn',
+            'est_Latn',
+            'fin_Latn',
+            'fra_Latn',
+            'lug_Latn',
+            'kat_Geor',
+            'deu_Latn',
+            'ell_Grek',
+            'guj_Gujr',
+            'heb_Hebr',
+            'hin_Deva',
+            'hun_Latn',
+            'isl_Latn',
+            'ind_Latn',
+            'gle_Latn',
+            'ita_Latn',
+            'jpn_Jpan',
+            'kaz_Cyrl',
+            'kor_Hang',
+            None,
+            'lvs_Latn',
+            'lit_Latn',
+            'mkd_Cyrl',
+            'msa_Latn',
+            'mri_Latn',
+            'mar_Deva',
+            'mon_Cyrl',
+            'nno_Latn',
+            'pes_Arab',
+            'pol_Latn',
+            'por_Latn',
+            'pan_Guru',
+            'ron_Latn',
+            'rus_Cyrl',
+            'srp_Cyrl',
+            'sna_Latn',
+            'slk_Latn',
+            'slv_Latn',
+            'som_Latn',
+            'sot_Latn',
+            'spa_Latn',
+            'swh_Latn',
+            'swe_Latn',
+            'tgl_Latn',
+            'tam_Taml',
+            'tel_Telu',
+            'tha_Thai',
+            'tso_Latn',
+            'tsn_Latn',
+            'tur_Latn',
+            'ukr_Cyrl',
+            'urd_Arab',
+            'vie_Latn',
+            'cym_Latn',
+            'xho_Latn',
+            'yor_Latn',
+            'zul_Latn',
+        ]
 
     def detect(self, text: str) -> tuple[Language, Score]:
         """
@@ -44,11 +125,21 @@ class LanguageDetector:
         score (Score)
             the confidence score of the detected language
         """
-        labels, scores = next(zip(*self.model.predict([text]), strict=True))
+        labels, scores = next(zip(*self.fast_model.predict([text]), strict=True))
+        fast_label: Language = labels[0][9:]  # pyright: ignore [reportAssignmentType]
+        fast_score = float(scores[0])
+
+        if fast_score >= 0.85:  # noqa: PLR2004
+            return fast_label, fast_score
+
+        confidence_value = self.lingua_model.compute_language_confidence_values(text)[0]
+
+        if (confidence := confidence_value.value) <= 0.1:  # noqa: PLR2004
+            return fast_label, fast_score
 
         return (
-            labels[0][9:],  # pyright: ignore [reportReturnType]
-            float(scores[0]),
+            self.lingua_languages[confidence_value.language.value],  # pyright: ignore [reportReturnType]
+            confidence,
         )
 
 
@@ -74,4 +165,7 @@ def get_language_detector(repository: str, *, stub: bool) -> LanguageDetector:
     if stub:
         return create_autospec(LanguageDetector)
 
-    return LanguageDetector(load_model(huggingface_file_download(repository, 'model.bin')))
+    return LanguageDetector(
+        load_model(huggingface_file_download(repository, 'model.bin')),
+        LanguageDetectorBuilder().from_all_languages_without(LinguaLanguage.LATIN).build(),
+    )
