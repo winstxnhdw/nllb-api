1e34f3495c0ef2e27af7ab373ac54f091f881208
winstxnhdw <winstxnhdw@gmail.com>
2025-09-13 01:59:03 +0800

    chore: get unused port

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,6 @@
 from os import environ as env
+from random import randint
+from socket import AF_INET, SOCK_STREAM, socket
 from subprocess import run
 
 from granian.constants import Interfaces
@@ -31,6 +33,25 @@ def main() -> None:
     granian.serve()
 
 
+def get_oci() -> str:
+    return 'docker'
+
+
+def get_unused_port() -> int:
+    port = 7860
+
+    with socket(AF_INET, SOCK_STREAM) as client:
+        while True:
+            try:
+                client.bind(('0.0.0.0', port))
+
+            except OSError:
+                port = randint(7860, 7999)  # noqa: S311
+
+            else:
+                return port
+
+
 def stub() -> None:
     env['STUB_TRANSLATOR'] = 'True'
     env['STUB_LANGUAGE_DETECTOR'] = 'True'
@@ -38,18 +59,23 @@ def stub() -> None:
 
 
 def cpu() -> None:
-    docker_build = ['docker', 'build', '-t', 'nllb-api', '.']
+    oci = get_oci()
+    docker_build = [oci, 'build', '-f', 'Dockerfile.build', '-t', 'nllb-api', '.']
+
+    port = get_unused_port()
     docker_run = [
-        'docker',
+        oci,
         'run',
         '--init',
         '--rm',
+        '-e',
+        f'SERVER_PORT={port}',
         '-e',
         'TRANSLATOR_THREADS=4',
         '-e',
         'AUTH_TOKEN=Test',
         '-p',
-        '7860:7860',
+        f'{port}:{port}',
         'nllb-api',
     ]
 
@@ -58,20 +84,25 @@ def cpu() -> None:
 
 
 def gpu() -> None:
-    docker_build = ['docker', 'build', '--build-arg', 'USE_CUDA=1', '-f', 'Dockerfile.build', '-t', 'nllb-api', '.']
+    oci = get_oci()
+    docker_build = [oci, 'build', '--build-arg', 'USE_CUDA=1', '-f', 'Dockerfile.build', '-t', 'nllb-api', '.']
+
+    port = get_unused_port()
     docker_run = [
-        'docker',
+        oci,
         'run',
         '--init',
         '--rm',
         '--gpus',
         'all',
         '-e',
+        f'SERVER_PORT={port}',
+        '-e',
         'TRANSLATOR_THREADS=4',
         '-e',
         'AUTH_TOKEN=Test',
         '-p',
-        '7860:7860',
+        f'{port}:{port}',
         'nllb-api',
     ]
 
@@ -80,8 +111,9 @@ def gpu() -> None:
 
 
 def huggingface() -> None:
-    docker_build = ['docker', 'build', '-t', 'nllb-api', '.']
-    docker_run = ['docker', 'run', '--init', '--rm', '-p', '7860:7860', 'nllb-api']
+    oci = get_oci()
+    docker_build = [oci, 'build', '-t', 'nllb-api', '.']
+    docker_run = [oci, 'run', '--init', '--rm', '-p', f'7860:{get_unused_port()}', 'nllb-api']
 
     run(docker_build, check=True)
     run(docker_run, check=True)
