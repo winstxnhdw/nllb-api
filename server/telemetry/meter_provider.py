b9bca961fc9a13917c693b39714d13007dc8ea25
winstxnhdw <winstxnhdw@gmail.com>
2025-10-15 19:12:51 +0800

    feat: add system metrics

--- server/telemetry/meter_provider.py
+++ server/telemetry/meter_provider.py
@@ -1,4 +1,5 @@
 from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
+from opentelemetry.instrumentation.system_metrics import SystemMetricsInstrumentor
 from opentelemetry.metrics import set_meter_provider
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
@@ -24,9 +25,32 @@ def get_meter_provider(*, otlp_service_name: str, otlp_service_instance_id: str)
     meter_provider (MeterProvider)
         the configured MeterProvider instance
     """
+    system_metrics_config = {
+        'system.cpu.time': ['idle', 'user', 'system', 'irq'],
+        'system.cpu.utilization': ['idle', 'user', 'system', 'irq'],
+        'system.memory.usage': ['used', 'free', 'cached'],
+        'system.memory.utilization': ['used', 'free', 'cached'],
+        'system.swap.usage': ['used', 'free'],
+        'system.swap.utilization': ['used', 'free'],
+        'system.disk.io': ['read', 'write'],
+        'system.disk.time': ['read', 'write'],
+        'system.network.dropped.packets': ['transmit', 'receive'],
+        'system.network.errors': ['transmit', 'receive'],
+        'system.network.io': ['transmit', 'receive'],
+        'system.network.connections': ['family', 'type'],
+        'system.thread_count': None,
+        'process.context_switches': ['involuntary', 'voluntary'],
+        'process.open_file_descriptor.count': None,
+        'process.runtime.gc_count': None,
+        'cpython.gc.collections': None,
+        'cpython.gc.collected_objects': None,
+        'cpython.gc.uncollectable_objects': None,
+    }
+
     resource = Resource({SERVICE_NAME: otlp_service_name, SERVICE_INSTANCE_ID: otlp_service_instance_id})
     merged_resource = resource.merge(OTELResourceDetector().detect())
     meter_provider = MeterProvider([PeriodicExportingMetricReader(OTLPMetricExporter())], merged_resource)
+    SystemMetricsInstrumentor(config=system_metrics_config).instrument(meter_provider=meter_provider)
     set_meter_provider(meter_provider)
 
     return meter_provider
