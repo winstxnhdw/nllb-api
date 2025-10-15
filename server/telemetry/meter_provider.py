08423f5413a75c20d68a23efb046e57b9bcddb5a
winstxnhdw <winstxnhdw@gmail.com>
2025-10-15 19:49:40 +0800

    chore: ignore all system metrics

--- server/telemetry/meter_provider.py
+++ server/telemetry/meter_provider.py
@@ -26,25 +26,22 @@ def get_meter_provider(*, otlp_service_name: str, otlp_service_instance_id: str)
         the configured MeterProvider instance
     """
     system_metrics_config = {
-        'system.cpu.time': ['idle', 'user', 'system', 'irq'],
-        'system.cpu.utilization': ['idle', 'user', 'system', 'irq'],
-        'system.memory.usage': ['used', 'free', 'cached'],
-        'system.memory.utilization': ['used', 'free', 'cached'],
-        'system.swap.usage': ['used', 'free'],
-        'system.swap.utilization': ['used', 'free'],
-        'system.disk.io': ['read', 'write'],
-        'system.disk.time': ['read', 'write'],
-        'system.network.dropped.packets': ['transmit', 'receive'],
-        'system.network.errors': ['transmit', 'receive'],
-        'system.network.io': ['transmit', 'receive'],
-        'system.network.connections': ['family', 'type'],
-        'system.thread_count': None,
         'process.context_switches': ['involuntary', 'voluntary'],
+        'process.cpu.time': ['user', 'system'],
+        'process.cpu.utilization': None,
+        'process.memory.usage': None,
+        'process.memory.virtual': None,
         'process.open_file_descriptor.count': None,
+        'process.thread.count': None,
+        'process.runtime.memory': ['rss', 'vms'],
+        'process.runtime.cpu.time': ['user', 'system'],
         'process.runtime.gc_count': None,
         'cpython.gc.collections': None,
         'cpython.gc.collected_objects': None,
         'cpython.gc.uncollectable_objects': None,
+        'process.runtime.thread_count': None,
+        'process.runtime.cpu.utilization': None,
+        'process.runtime.context_switches': ['involuntary', 'voluntary'],
     }
 
     resource = Resource({SERVICE_NAME: otlp_service_name, SERVICE_INSTANCE_ID: otlp_service_instance_id})
