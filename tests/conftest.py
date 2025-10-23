da5dee1cc28dcc345f42d028f18e937f9dca6b02
winstxnhdw <winstxnhdw@gmail.com>
2025-10-23 23:23:20 +0800

    test: use `pytest-xdist` and distribute by scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,49 +3,53 @@ from typing import Literal
 
 from litestar import Litestar
 from litestar.testing import AsyncTestClient
-from pytest import fixture
+from pytest import Config, fixture
 
 from server.app import app
-from server.config import Config
+from server.config import Config as ServerConfig
 
 
-def client_factory(config: Config, *, no_lifespans: bool) -> AsyncTestClient[Litestar]:
+def pytest_configure(config: Config) -> None:
+    config.addinivalue_line("markers", "flaky: marks tests as flaky (deselect with '-m \"not flaky\"')")
+
+
+def client_factory(config: ServerConfig, *, no_lifespans: bool) -> AsyncTestClient[Litestar]:
     litestar = app(config)
 
     if no_lifespans:
         litestar._lifespan_managers.clear()
 
-    return AsyncTestClient(app=litestar, backend_options={'use_uvloop': True})
+    return AsyncTestClient(app=litestar, backend_options={"use_uvloop": True})
 
 
-@fixture(scope='session')
+@fixture(scope="session")
 def auth_token() -> str:
-    return 'test_token'
+    return "test_token"
 
 
-@fixture(scope='session')
-def anyio_backend() -> tuple[Literal['asyncio', 'trio'], dict[str, bool]]:
-    return 'asyncio', {'use_uvloop': True}
+@fixture(scope="session")
+def anyio_backend() -> tuple[Literal["asyncio", "trio"], dict[str, bool]]:
+    return "asyncio", {"use_uvloop": True}
 
 
 @fixture
 async def client(auth_token: str) -> AsyncIterator[AsyncTestClient[Litestar]]:
-    config = Config()
+    config = ServerConfig()
     config.auth_token = auth_token
 
-    async with AsyncTestClient(app=app(config), backend_options={'use_uvloop': True}) as client:
+    async with AsyncTestClient(app=app(config), backend_options={"use_uvloop": True}) as client:
         yield client
 
 
 @fixture
-async def client_factory_without_lifespans() -> Callable[[Config], AsyncTestClient[Litestar]]:
+async def client_factory_without_lifespans() -> Callable[[ServerConfig], AsyncTestClient[Litestar]]:
     return lambda config: client_factory(config, no_lifespans=True)
 
 
-@fixture(scope='session')
+@fixture(scope="session")
 async def session_client(auth_token: str) -> AsyncIterator[AsyncTestClient[Litestar]]:
-    config = Config()
+    config = ServerConfig()
     config.auth_token = auth_token
 
-    async with AsyncTestClient(app=app(config), backend_options={'use_uvloop': True}) as client:
+    async with AsyncTestClient(app=app(config), backend_options={"use_uvloop": True}) as client:
         yield client
