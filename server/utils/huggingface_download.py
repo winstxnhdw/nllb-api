59fdfb3d333790f64f299a60d7e2a017c817723b
winstxnhdw <winstxnhdw@gmail.com>
2026-08-22 02:39:38 +0800

    fix: mitigate corrupted tree cache error

--- server/utils/huggingface_download.py
+++ server/utils/huggingface_download.py
@@ -1,4 +1,6 @@
-from huggingface_hub import snapshot_download
+from pathlib import Path
+
+from huggingface_hub import snapshot_download, try_to_load_from_cache
 
 from server.utils.has_internet_access import has_internet_access
 
@@ -17,6 +19,9 @@ def huggingface_download(repository: str) -> str:
     -------
     repository_path (str) : local path to the downloaded repository
     """
+    if isinstance(cached_file := try_to_load_from_cache(repository, "tokenizer.json"), str):
+        return str(Path(cached_file).parent)
+
     return snapshot_download(
         repository,
         local_files_only=not has_internet_access(repository),
--- server/utils/huggingface_file_download.py
+++ server/utils/huggingface_file_download.py
@@ -1,4 +1,4 @@
-from huggingface_hub import hf_hub_download
+from huggingface_hub import hf_hub_download, try_to_load_from_cache
 
 from server.utils.has_internet_access import has_internet_access
 
@@ -7,16 +7,20 @@ def huggingface_file_download(repository: str, file: str) -> str:
     """
     Summary
     -------
-    download the huggingface model
+    download a file from the huggingface hub
 
     Parameters
     ----------
     repository (str) : the name of the Hugging Face repository
+    file (str) : the name of the file to download
 
     Returns
     -------
-    repository_path (str) : local path to the downloaded repository
+    file_path (str) : local path to the downloaded file
     """
+    if isinstance(cached_file := try_to_load_from_cache(repository, file), str):
+        return cached_file
+
     return hf_hub_download(
         repository,
         file,
